f3eecb2e64c8c58174cdf1e9af0d0c0f8e4694d4
Keelen Fisher <keelencarrera@gmail.com>
2022-07-25 14:02:26 -0500

    Added notes from class in class11

--- class-11/demo/media-click-tracker/js/goat.js
+++ class-11/demo/media-click-tracker/js/goat.js
@@ -1,90 +1,250 @@
 'use strict';
-
+// ************** IN CLASS NOTES *****************
 // global varriables
 
-let goatContainer = document.querySelector('section');
-let resultButton = document.querySelector('section + div');
-let image1 = document.querySelector('section img:first-child');
-let image2 = document.querySelector('section img:nth-child(2)');
+console.log('hey world Hey!');
 
-let clicks = 0;
-let maxClicksAllowed = 9;
+// ******** Global Variables******************
+let totalVotes = 15;
+let allGoats = []
 
-// functional logic
 
-function Goat(name, src) {
+// ********** DOM REFERENCES*****************
+//  YOU NEED TO CREATE 5 REFERENCES: TWO IMG, DIV AND UL, THEN SECTION.
+let imgContainer = document.getElementById('img-container');
+let imgOne = document.getElementById('img-one');
+let imgTwo = document.getElementById('img-two');
+
+let resultBtn = document.getElementById('show-results-btn');
+let resultsList = document.getElementById('results-list')
+
+
+// ********* CONSTRUCT FUNCTION **************
+
+function Goat(name, photoExtension = 'jpg'){
   this.name = name;
-  this.src = src;
+  this.photo = `img/${name}.${photoExtension}`;
   this.views = 0;
-  this.clicks = 0;
-  Goat.allGoatsArray.push(this);
+  this.votes = 0;
+
+  allGoats.push(this);
+
 }
 
-Goat.allGoatsArray = [];
 
-function getRandomNumber() {
-  return Math.floor(Math.random() * Goat.allGoatsArray.length);
+// ***************** OBJECT CREATION ********************
+
+new Goat('bunny-goat', 'png');
+new Goat('cool-goat');
+new Goat('crusin-goat');
+new Goat('float-your-goat');
+new Goat('goat-out-of-hand');
+new Goat('kissing-goat');
+new Goat('sassy-goat');
+new Goat('smiling-goat');
+new Goat('sweater-goat');
+
+
+// *************** Helper FUNCTIONS *******************
+
+function randomIndexGenerator(){
+  return Math.floor(Math.random() * allGoats.length);
+  // min - 0
+  // max - array.length-1
 }
 
-function renderGoats() {
-  // call the getRandomNumber
-  let goat1 = getRandomNumber();
-  let goat2 = getRandomNumber();
+function renderImg(){
+  //  Just get 2 images on the page:
+ let imgOneIndex = randomIndexGenerator();
+ let imgTwoIndex = randomIndexGenerator();
 
-  while (goat1 === goat2) {
-    goat2 = getRandomNumber();
-  }
-  image1.src = Goat.allGoatsArray[goat1].src;
-  image2.src = Goat.allGoatsArray[goat2].src;
-  image1.alt = Goat.allGoatsArray[goat1].name;
-  image2.alt = Goat.allGoatsArray[goat2].name;
-  Goat.allGoatsArray[goat1].views++;
-  Goat.allGoatsArray[goat2].views++;
+//  make sure they are unique each round 
+
+while(imgOneIndex === imgTwoIndex){
+  imgTwoIndex = randomIndexGenerator();
+
 }
 
-function handleGoatClick(event) {
-  if (event.target === goatContainer) {
-    alert('Please click on an image');
-  }
-  clicks++;
-  let clickGoat = event.target.alt;
-  for (let i = 0; i < Goat.allGoatsArray.length; i++) {
-    if (clickGoat === Goat.allGoatsArray[i].name) {
-      Goat.allGoatsArray[i].clicks++;
-      break;
+// Hint for lab 11: use a container to store your 3 index numbers and do validation against that collectio/container 
+  imgOne.src = allGoats[imgOneIndex].photo;
+  imgOne.alt = allGoats[imgOneIndex].name;
+  allGoats[imgOneIndex].views++;
+  imgTwo.src = allGoats[imgTwoIndex].photo;
+  imgTwo.alt - allGoats[imgTwoIndex].name;
+  allGoats[imgTwoIndex].views++;
+
+}
+
+// **************** INVOKE ***************
+renderImg();
+
+
+
+// *********** EVENT HANDLERS ***********************
+//  click- on the imgs -  rerender new imaged(increase the view on the goats that are rendered) - count the vote of the goat that was clicked/ lower your total number of votes 
+function handleClick(event){
+  let imgClicked = event.target.alt;
+  for(let i = 0; i < allGoats.length; i++){
+    if(imgClicked === allGoats[i].name){
+      allGoats[i].votes++;
     }
   }
-  if (clicks === maxClicksAllowed) {
-    goatContainer.removeEventListener('click', handleGoatClick);
-    // give the button an event lister and styles so the user
-    // knows its an active button:
-    resultButton.addEventListener('click', renderResults);
-    resultButton.className = 'clicks-allowed';
-    goatContainer.className = 'no-voting';
-  } else {
-    renderGoats();
+  totalVotes--;
+  renderImg();
+
+  console.log(imgClicked);
+
+  if(totalVotes === 0){
+    imgContainer.removeEventListener('click', handleClick);
   }
-}
+  
+ }
 
-function renderResults() {
-  let ul = document.querySelector('ul');
-  for (let i = 0; i < Goat.allGoatsArray.length; i++) {
-    let li = document.createElement('li')
-    li.textContent = `${Goat.allGoatsArray[i].name} had ${Goat.allGoatsArray[i].views} view and was clicked ${Goat.allGoatsArray[i].clicks} times.`;
-    ul.appendChild(li);
+ function handleShowResults(){
+  if(totalVotes === 0){
+    for(let i = 0; i < allGoats.length; i++){
+      let liElem = document.createElement('li');
+      liElem.textContent = `${allGoats[1].name}: views: ${allGoats[i].views}, votes: ${allGoats[i].votes}`;
+      resultsList.appendChild(liElem);
+    }
+    resultsBtn.removeEventListener('click', handleShowResults);
   }
-}
+ }
+// ************** EVENT LISTENERS ******************
 
-// executable code
 
-new Goat('Cruising Goat', './images/cruisin-goat.jpg');
-new Goat('Float Your Goat', './images/float-your-goat.jpg');
-new Goat('Goat Out of Hand', './images/goat-out-of-hand.jpg');
-new Goat('Kissing Goat', './images/kissing-goat.jpg');
-new Goat('Sassy Goat', './images/sassy-goat.jpg');
-new Goat('Smiling Goat', './images/smiling-goat.jpg');
-new Goat('Sweater Goat', './images/sweater-goat.jpg');
+imgContainer.addEventListener('click', handleClick);
 
-renderGoats();
+resultsBtn.addEventListener('click', handleShowResults);
 
-goatContainer.addEventListener('click', handleGoatClick);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let goatContainer = document.querySelector('section');
+// let resultButton = document.querySelector('section + div');
+// let image1 = document.querySelector('section img:first-child');
+// let image2 = document.querySelector('section img:nth-child(2)');
+
+// let clicks = 0;
+// let maxClicksAllowed = 9;
+
+// // functional logic
+
+// function Goat(name, src) {
+//   this.name = name;
+//   this.src = src;
+//   this.views = 0;
+//   this.clicks = 0;
+//   Goat.allGoatsArray.push(this);
+// }
+
+// Goat.allGoatsArray = [];
+
+// function getRandomNumber() {
+//   return Math.floor(Math.random() * Goat.allGoatsArray.length);
+// }
+
+// function renderGoats() {
+//   // call the getRandomNumber
+//   let goat1 = getRandomNumber();
+//   let goat2 = getRandomNumber();
+
+//   while (goat1 === goat2) {
+//     goat2 = getRandomNumber();
+//   }
+//   image1.src = Goat.allGoatsArray[goat1].src;
+//   image2.src = Goat.allGoatsArray[goat2].src;
+//   image1.alt = Goat.allGoatsArray[goat1].name;
+//   image2.alt = Goat.allGoatsArray[goat2].name;
+//   Goat.allGoatsArray[goat1].views++;
+//   Goat.allGoatsArray[goat2].views++;
+// }
+
+// function handleGoatClick(event) {
+//   if (event.target === goatContainer) {
+//     alert('Please click on an image');
+//   }
+//   clicks++;
+//   let clickGoat = event.target.alt;
+//   for (let i = 0; i < Goat.allGoatsArray.length; i++) {
+//     if (clickGoat === Goat.allGoatsArray[i].name) {
+//       Goat.allGoatsArray[i].clicks++;
+//       break;
+//     }
+//   }
+//   if (clicks === maxClicksAllowed) {
+//     goatContainer.removeEventListener('click', handleGoatClick);
+//     // give the button an event lister and styles so the user
+//     // knows its an active button:
+//     resultButton.addEventListener('click', renderResults);
+//     resultButton.className = 'clicks-allowed';
+//     goatContainer.className = 'no-voting';
+//   } else {
+//     renderGoats();
+//   }
+// }
+
+// function renderResults() {
+//   let ul = document.querySelector('ul');
+//   for (let i = 0; i < Goat.allGoatsArray.length; i++) {
+//     let li = document.createElement('li')
+//     li.textContent = `${Goat.allGoatsArray[i].name} had ${Goat.allGoatsArray[i].views} view and was clicked ${Goat.allGoatsArray[i].clicks} times.`;
+//     ul.appendChild(li);
+//   }
+// }
+
+// // executable code
+
+// new Goat('Cruising Goat', './images/cruisin-goat.jpg');
+// new Goat('Float Your Goat', './images/float-your-goat.jpg');
+// new Goat('Goat Out of Hand', './images/goat-out-of-hand.jpg');
+// new Goat('Kissing Goat', './images/kissing-goat.jpg');
+// new Goat('Sassy Goat', './images/sassy-goat.jpg');
+// new Goat('Smiling Goat', './images/smiling-goat.jpg');
+// new Goat('Sweater Goat', './images/sweater-goat.jpg');
+
+// renderGoats();
+
+// goatContainer.addEventListener('click', handleGoatClick);
